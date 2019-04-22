9e8d97f7c68037a42357cf507c98484dffeeee79
swatantra <swatantra.kumar8@gmail.com>
2019-04-23 01:52:19 +0530

    fixes added

--- controllers/customers.js
+++ controllers/customers.js
@@ -103,6 +103,20 @@ exports.loginCustomer = (req, res, next) => {
 exports.loginFbCustomer = (req, res, next) => {
 
     const access_token = req.body.access_token;
+
+    passport.authenticate('facebook-token', {session: false}), function(req, res, next) {
+        
+        if (!req.user) {
+          return res.send(401, 'User Not Authenticated');
+        }
     
+        // prepare token for API
+        req.auth = {
+          id: req.user.id
+        };
+    
+        next();
+      };
+
 
 };
