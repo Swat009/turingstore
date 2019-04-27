42804a19224fbc3d2afb892e61bbafdc8f7548a7
swatantra <swatantra.kumar8@gmail.com>
2019-04-27 17:28:49 +0530

    host bug fix

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, { 
     dialect: 'mysql',
-    host: 'localhost'
+    host: process.env.HOST
 
 });
 
